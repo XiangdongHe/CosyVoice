13162fe9b53f218c67c8bb10fd49b99cf8568490
HeXiangdong <545466093@qq.com>
2025-04-07 15:17:35 +0800

    refactor:融合了VRM

--- runtime/python/fastapi/server.py
+++ runtime/python/fastapi/server.py
@@ -12,15 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import re
 import sys
+import time
 import argparse
 import logging
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
-from fastapi import FastAPI, UploadFile, Form, File
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, HTTPException, Form
+from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 import uvicorn
 import numpy as np
+from pydantic_settings import BaseSettings
+from pydantic import BaseModel, Field
+from funasr import AutoModel
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append('{}/../../..'.format(ROOT_DIR))
 sys.path.append('{}/../../../third_party/Matcha-TTS'.format(ROOT_DIR))
@@ -54,6 +62,282 @@ async def inference_instruct2(content: str = Form()):
 async def inference_instruct2(content: str):
     model_output = cosyvoice.inference_instruct2(content, instruct_text, prompt_speech_16k)
     return StreamingResponse(generate_data(model_output))
+
+
+class Config(BaseSettings):
+    sv_thr: float = Field(0.3, description="Speaker verification threshold")
+    chunk_size_ms: int = Field(300, description="Chunk size in milliseconds")
+    sample_rate: int = Field(16000, description="Sample rate in Hz")
+    bit_depth: int = Field(16, description="Bit depth")
+    channels: int = Field(1, description="Number of audio channels")
+    avg_logprob_thr: float = Field(-0.25, description="average logprob threshold")
+
+
+config = Config()
+
+emo_dict = {
+    "<|HAPPY|>": "😊",
+    "<|SAD|>": "😔",
+    "<|ANGRY|>": "😡",
+    "<|NEUTRAL|>": "",
+    "<|FEARFUL|>": "😰",
+    "<|DISGUSTED|>": "🤢",
+    "<|SURPRISED|>": "😮",
+}
+
+event_dict = {
+    "<|BGM|>": "🎼",
+    "<|Speech|>": "",
+    "<|Applause|>": "👏",
+    "<|Laughter|>": "😀",
+    "<|Cry|>": "😭",
+    "<|Sneeze|>": "🤧",
+    "<|Breath|>": "",
+    "<|Cough|>": "🤧",
+}
+
+emoji_dict = {
+    "<|nospeech|><|Event_UNK|>": "❓",
+    "<|zh|>": "",
+    "<|en|>": "",
+    "<|yue|>": "",
+    "<|ja|>": "",
+    "<|ko|>": "",
+    "<|nospeech|>": "",
+    "<|HAPPY|>": "😊",
+    "<|SAD|>": "😔",
+    "<|ANGRY|>": "😡",
+    "<|NEUTRAL|>": "",
+    "<|BGM|>": "🎼",
+    "<|Speech|>": "",
+    "<|Applause|>": "👏",
+    "<|Laughter|>": "😀",
+    "<|FEARFUL|>": "😰",
+    "<|DISGUSTED|>": "🤢",
+    "<|SURPRISED|>": "😮",
+    "<|Cry|>": "😭",
+    "<|EMO_UNKNOWN|>": "",
+    "<|Sneeze|>": "🤧",
+    "<|Breath|>": "",
+    "<|Cough|>": "😷",
+    "<|Sing|>": "",
+    "<|Speech_Noise|>": "",
+    "<|withitn|>": "",
+    "<|woitn|>": "",
+    "<|GBG|>": "",
+    "<|Event_UNK|>": "",
+}
+
+lang_dict = {
+    "<|zh|>": "<|lang|>",
+    "<|en|>": "<|lang|>",
+    "<|yue|>": "<|lang|>",
+    "<|ja|>": "<|lang|>",
+    "<|ko|>": "<|lang|>",
+    "<|nospeech|>": "<|lang|>",
+}
+
+emo_set = {"😊", "😔", "😡", "😰", "🤢", "😮"}
+event_set = {"🎼", "👏", "😀", "😭", "🤧", "😷", }
+
+
+def format_str(s):
+    for sptk in emoji_dict:
+        s = s.replace(sptk, emoji_dict[sptk])
+    return s
+
+
+def format_str_v2(s):
+    sptk_dict = {}
+    for sptk in emoji_dict:
+        sptk_dict[sptk] = s.count(sptk)
+        s = s.replace(sptk, "")
+    emo = "<|NEUTRAL|>"
+    for e in emo_dict:
+        if sptk_dict[e] > sptk_dict[emo]:
+            emo = e
+    for e in event_dict:
+        if sptk_dict[e] > 0:
+            s = event_dict[e] + s
+    s = s + emo_dict[emo]
+
+    for emoji in emo_set.union(event_set):
+        s = s.replace(" " + emoji, emoji)
+        s = s.replace(emoji + " ", emoji)
+    return s.strip()
+
+
+def format_str_v3(s):
+    def get_emo(s):
+        return s[-1] if s[-1] in emo_set else None
+
+    def get_event(s):
+        return s[0] if s[0] in event_set else None
+
+    s = s.replace("<|nospeech|><|Event_UNK|>", "❓")
+    for lang in lang_dict:
+        s = s.replace(lang, "<|lang|>")
+    s_list = [format_str_v2(s_i).strip(" ") for s_i in s.split("<|lang|>")]
+    new_s = " " + s_list[0]
+    cur_ent_event = get_event(new_s)
+    for i in range(1, len(s_list)):
+        if len(s_list[i]) == 0:
+            continue
+        if get_event(s_list[i]) == cur_ent_event and get_event(s_list[i]) != None:
+            s_list[i] = s_list[i][1:]
+        # else:
+        cur_ent_event = get_event(s_list[i])
+        if get_emo(s_list[i]) != None and get_emo(s_list[i]) == get_emo(new_s):
+            new_s = new_s[:-1]
+        new_s += s_list[i].strip().lstrip()
+    new_s = new_s.replace("The.", " ")
+    return new_s.strip()
+
+
+def contains_chinese_english_number(s: str) -> bool:
+    # Check if the string contains any Chinese character, English letter, or Arabic number
+    return bool(re.search(r'[\u4e00-\u9fffA-Za-z0-9]', s))
+
+
+model_asr = AutoModel(
+    model=os.environ.get("MODEL_ASR_DIR"),
+    trust_remote_code=True,
+    remote_code="./model.py",
+    device="cuda:0",
+    disable_update=True
+)
+
+model_vad = AutoModel(
+    model=os.environ.get("MODEL_VAD_DIR"),
+    model_revision="v2.0.4",
+    disable_pbar=True,
+    max_end_silence_time=500,
+    # speech_noise_thres=0.6,
+    disable_update=True,
+)
+
+
+def asr(audio, lang, cache, use_itn=False):
+    start_time = time.time()
+    result = model_asr.generate(
+        input=audio,
+        cache=cache,
+        language=lang.strip(),
+        use_itn=use_itn,
+        batch_size_s=60,
+    )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return result
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.exception_handler(Exception)
+async def custom_exception_handler(request: Request, exc: Exception):
+    if isinstance(exc, HTTPException):
+        status_code = exc.status_code
+        message = exc.detail
+        data = ""
+    elif isinstance(exc, RequestValidationError):
+        status_code = HTTP_422_UNPROCESSABLE_ENTITY
+        message = "Validation error: " + str(exc.errors())
+        data = ""
+    else:
+        status_code = 500
+        message = "Internal server error: " + str(exc)
+        data = ""
+
+    return JSONResponse(
+        status_code=status_code,
+        content=TranscriptionResponse(
+            code=status_code,
+            msg=message,
+            data=data
+        ).model_dump()
+    )
+
+
+# Define the response model
+class TranscriptionResponse(BaseModel):
+    code: int
+    info: str
+    data: str
+
+
+@app.websocket("/ws/transcribe")
+async def websocket_endpoint(websocket: WebSocket):
+    try:
+        lang = 'zh'
+        await websocket.accept()
+        chunk_size = int(config.chunk_size_ms * config.sample_rate / 1000)
+        audio_buffer = np.array([], dtype=np.float32)
+        audio_vad = np.array([], dtype=np.float32)
+
+        cache = {}
+        cache_asr = {}
+        last_vad_beg = last_vad_end = -1
+        offset = 0
+
+        buffer = b""
+        while True:
+            data = await websocket.receive_bytes()
+
+            buffer += data
+            if len(buffer) < 2:
+                continue
+
+            audio_buffer = np.append(
+                audio_buffer,
+                np.frombuffer(buffer[:len(buffer) - (len(buffer) % 2)], dtype=np.int16).astype(np.float32) / 32767.0
+            )
+
+            buffer = buffer[len(buffer) - (len(buffer) % 2):]
+
+            while len(audio_buffer) >= chunk_size:
+                chunk = audio_buffer[:chunk_size]
+                audio_buffer = audio_buffer[chunk_size:]
+                audio_vad = np.append(audio_vad, chunk)
+
+                res = model_vad.generate(input=chunk, cache=cache, is_final=False, chunk_size=config.chunk_size_ms)
+                if len(res[0]["value"]):
+                    vad_segments = res[0]["value"]
+                    for segment in vad_segments:
+                        if segment[0] > -1:  # speech begin
+                            last_vad_beg = segment[0]
+                        if segment[1] > -1:  # speech end
+                            last_vad_end = segment[1]
+                        if last_vad_beg > -1 and last_vad_end > -1:
+                            last_vad_beg -= offset
+                            last_vad_end -= offset
+                            offset += last_vad_end
+                            beg = int(last_vad_beg * config.sample_rate / 1000)
+                            end = int(last_vad_end * config.sample_rate / 1000)
+                            result = asr(audio_vad[beg:end], lang.strip(), cache_asr, True)
+                            audio_vad = audio_vad[end:]
+                            last_vad_beg = last_vad_end = -1
+                            if result is not None:
+                                await websocket.send_text(format_str_v3(result[0]['text']))
+                                time.sleep(1)
+                                await websocket.send_text('\n')
+
+
+    except Exception as e:
+        await websocket.close()
+    finally:
+        audio_buffer = np.array([], dtype=np.float32)
+        audio_vad = np.array([], dtype=np.float32)
+        cache.clear()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
